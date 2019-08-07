3cd2958ab5edb601efbe9a1ab51afd2fd8316d05
Xinyi Wu <xxinyiwho@gmail.com>
2019-08-07 22:11:55 +0200

    unfinished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seeding..."
+
+Flat.destroy_all
+
+10.times do
+  flats = Flat.create!({
+    name: Faker::FunnyName.four_word_name,
+    address: Faker::Address.street_name,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: %w(65 80 83 89 90 95 100 110 120 130).sample,
+    number_of_guest: %w(1 2 3 4).sample
+  })
+end
+
+puts "Done"
